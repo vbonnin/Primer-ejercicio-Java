21e27b1e91b6ed145c95a7319a1122b6b0a392cd
vbonnin <vbonninpalou@cifpfbmoll.eu>
2023-02-13 10:39:20 +0100

    cambios ejercicio H

--- ejercicioH.java
+++ ejercicioH.java
@@ -3,32 +3,41 @@
 import java.util.Scanner;
 
 public class ejercicioH {
+
     public static void ejercicio8() {
-        Scanner sc = new Scanner(System.in); 
+        Scanner sc = new Scanner(System.in);
         // Creamos un objeto de tipo Scanner para leer datos de entrada del usuario
-        System.out.print("Introduce una cadena: "); 
+        System.out.print("Introduce una cadena: ");
         // Pedimos al usuario que introduzca una cadena de caracteres
-        String cadena = sc.nextLine(); 
+        String cadena = sc.nextLine();
         // Creamos una variable para leer la cadena introducida por el usuario
-        System.out.print("Introduce una vocal: "); 
-        // Pedimos al usuario que introduzca una vocal
-        char vocal = sc.nextLine().charAt(0); 
-        // Creamos una variable para leer la vocal introducida por el usuario y tomamos solo el primer carácter
+        System.out.print("Introduce una vocal: ");
+         // Pedimos al usuario que introduzca una vocal
+        char vocal = sc.nextLine().charAt(0);
+        // Creamos una variable para leer la vocal introducida por el usuario y tomamos solo el primer caracter
 
-        StringBuilder sb = new StringBuilder(cadena); 
-        // Creamos un objeto de tipo StringBuilder a partir de la cadena introducida
-
-        for (int i = 0; i < sb.length(); i++) { 
-            // Iteramos un bucle que recorra cada caracter de la cadena
-            char cambio = sb.charAt(i); 
-            // Creamouns una variable obtienendo el caracter actual
+        
+        String nuevaCadena = "";
+        // Creamos una cadena vacía donde iremos concatenando los caracteres modificados
+        
+        for (int i = 0; i < cadena.length(); i++) {
+             // Iteramos el bucle sobre cada caracter de la cadena original
+            
+            char cambio = cadena.charAt(i);
+            // Obtenemos el caracter actual
+            // Si el caracter actual es una vocal, lo reemplazamos por la vocal introducida por el usuario
             if (cambio == 'a' || cambio == 'e' || cambio == 'i' || cambio == 'o' || cambio == 'u' || cambio == 'A' || cambio == 'E' || cambio == 'I' || cambio == 'O' || cambio == 'U') {
-                sb.setCharAt(i, vocal); 
-                // Si el caracter actual es una vocal, lo reemplaza por la vocal introducida por el usuario
+                nuevaCadena += vocal;
+                // Si el caracter actual es una vocal, lo reemplazamos por la vocal introducida por el usuario
+            } else {
+                // Si el caracter actual no es una vocal, lo concatenamos tal cual a la nueva cadena
+                nuevaCadena += cambio;
             }
         }
 
-        System.out.println("La cadena con las vocales sustituidas es: " + sb.toString()); 
-        // Mostramos la cadena con las vocales sustituidas
+       
+        System.out.println("La cadena con las vocales sustituidas es: " + nuevaCadena);
+        // Mostramos por pantalla la cadena con las vocales sustituidas
     }
 }
+
